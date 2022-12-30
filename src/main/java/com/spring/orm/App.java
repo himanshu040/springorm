d107d23497001bfddf94690265c9df54f83cb492
Himanshu Singh <Himanshu Singh>
2022-12-30 20:49:08 +0530

    spring orm crud

--- src/main/java/com/spring/orm/App.java
+++ src/main/java/com/spring/orm/App.java
@@ -1,5 +1,9 @@
 package com.spring.orm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,9 +20,102 @@ public class App
     {
        ApplicationContext context= new ClassPathXmlApplicationContext("com/spring/orm/config.xml");
        Studentdao studentdao=context.getBean("studentDao",Studentdao.class);
-       Student student=new Student(2324,"himanshu","Delhi");
-       
-       int r=studentdao.insert(student);
-       System.out.println(r);
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      
+      boolean go=true;
+      
+      while(go)
+      {
+    	 
+    	  System.out.println("Press 1 for add new student");
+          System.out.println("Press 2 for display all student");
+          System.out.println("Press 3 for get details of single student");
+          System.out.println("Press 4 for delete student");
+          System.out.println("Press 5 for update student");
+          System.out.println("Press 6 for exit");
+          
+          try
+          {
+        	  System.out.println("Enter choice:");
+        	  int input = Integer.parseInt(br.readLine());
+        	 
+        	  switch (input) {
+			case 1://add
+	        	  System.out.println("*****************************************************************************************");
+
+				  Student student=new Student();
+	        	  System.out.println("Enter userId:");
+	        	  int uid=Integer.parseInt(br.readLine());
+	        	  student.setStudentID(uid);
+	        	  System.out.println("Enter name:");
+	        	  String name=br.readLine();
+	        	  student.setStudentName(name);
+	        	  System.out.println("Enter city:");
+	        	  String address=br.readLine();
+	        	  student.setStudentCity(address);
+	        	 int r= studentdao.insert(student);
+	        	  System.out.println(r+" Student added");
+	        	  System.out.println("*****************************************************************************************");
+
+				break;
+			case 2://display all
+				System.out.println("*****************************************************************************************");
+				System.out.println("List of students:");
+				List<Student> students=studentdao.getAllStudent();
+				for(Student s:students)
+				{
+					System.out.println("Student id:"+s.getStudentID());
+					System.out.println("Student name:"+s.getStudentName());
+					System.out.println("Student city:"+s.getStudentCity());
+					System.out.println("----------------------------------------------------------------------------------------------");
+				}
+				System.out.println("*****************************************************************************************");
+				break;
+			case 3://detail
+				System.out.println("*****************************************************************************************");
+				System.out.println("Enter student Id to show details:");
+				int i=Integer.parseInt(br.readLine());
+				Student s=studentdao.getStudent(i);
+				if(s!=null)
+				{
+					 System.out.println("Student details student Id "+i);
+						System.out.println("Student id:"+s.getStudentID());
+						System.out.println("Student name:"+s.getStudentName());
+						System.out.println("Student city:"+s.getStudentCity());
+						System.out.println("----------------------------------------------------------------------------------------------");
+				}
+				else
+				{
+					System.out.println("No record found with student id "+i);
+				}
+				  
+				System.out.println("*****************************************************************************************");
+				break;
+			case 4://delete
+				System.out.println("*****************************************************************************************");
+				System.out.println("Enter student Id to Delete record:");
+				int id=Integer.parseInt(br.readLine());
+				studentdao.delete(id);
+				System.out.println("Delete record with id "+id);
+				System.out.println("*****************************************************************************************");
+				break;
+			case 5://update
+				break;
+			case 6:
+				go=false;
+                 break;
+				}
+        	  
+          }
+          catch(Exception e)
+          {
+        	  System.out.println("Invalid choice try again!!");
+        	  System.out.println(e.getMessage());
+          }
+    	  
+      }
+      System.out.println("Thank you for using!!");
+	  System.out.println("See you again!!"); 
+      
     }
 }
--- src/main/java/com/spring/orm/dao/Studentdao.java
+++ src/main/java/com/spring/orm/dao/Studentdao.java
@@ -1,5 +1,7 @@
 package com.spring.orm.dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.orm.hibernate5.HibernateTemplate;
@@ -16,6 +18,25 @@ public class Studentdao {
 	{
 		Integer i =(Integer)this.hibernateTemplate.save(student);
 		return 1;
+	}
+	
+	public Student getStudent(int stdId)
+	{
+		Student student=this.hibernateTemplate.get(Student.class,stdId);
+		return student;
+	}
+	
+	public List<Student> getAllStudent()
+	{
+		List<Student> students=this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+	
+	@Transactional
+	public void delete(int stdId)
+	{
+		Student student=this.hibernateTemplate.get(Student.class,stdId);
+		this.hibernateTemplate.delete(student);
 	}
 
 	public HibernateTemplate getHibernateTemplate() {
